12c5bc9827bee21f08a0303efeaf42af25a07517
Matt Harker <mattsharker@gmail.com>
2019-05-13 23:03:25 -0700

    First working version of FFA

--- include/firefly.h
+++ include/firefly.h
@@ -8,13 +8,20 @@ void firefly(Population** populations);
 void runFirefly(Population* population);
 
 void initializeFFO(Population* pop);
-void moveFirefly(Population* population, const int ff1, const int ff2);
 
-double equationOne(Population* population, const int r);
-double equationTwo(Population* population, const int r);
-double equationThree(Population* population, const int ff1, const int ff2);
-double equationFour(Population* population,  const int r, const int ff1, const int ff2, const int t, const int d);
+// equation 1
+double intensity(Population* population, const int ff, double r);
 
+// equation 2
+double equationTwo(Population* population, double r);
+
+// equation 3, returns the distance between two fireflies
+double ffDistance(Population* population, const int ff1, const int ff2);    //
+
+// equation 4
+void equationFour(Population* population, double* newPos, const int r, const int ff1, const int ff2);
+
+void addNewFirefly(Population* popualtion, double* newPos);
 
 
 #endif
--- src/firefly.cpp
+++ src/firefly.cpp
@@ -4,11 +4,14 @@
 #include <thread>
 
 #include "firefly.h"
+#include "runFuncs.h"
 
 using namespace std;
 
 void firefly(Population** pops)
 {
+    cout << "Starting firefly optimization\n";
+
     int funcs = pops[0]->getNumFuncs();
     thread* myThreads = new thread[funcs];
 /*
@@ -22,12 +25,23 @@ void firefly(Population** pops)
         runFirefly(pops[i]);
 
     delete [] myThreads;
+
+    cout << "Firefly optimization has completed\n";
 }
 
 
 void runFirefly(Population* pop)
 {
     // initialize the fireflies
+    initializeFFO(pop);
+    
+    // variables for the algorithm
+    double r;
+    double li1;
+    double li2;
+
+    // create an array to hold a temporary position
+    double* newPos = new double[pop->getSolutionSize()];
 
     // for each iteration
     for (int t = 0; t < pop->getExperimentations(); ++t)
@@ -38,15 +52,34 @@ void runFirefly(Population* pop)
             // for each firefly again
             for (int j = 0; j < pop->getPopSize(); ++j)
             {
-                // move firefly j towards firefly i (equation 3)
+// cout << "here1\n";
+                // get the distance (eq 3, 2)
+                r = ffDistance(pop, i, j);
 
-                // attractiveness varies with distance r via (equation 2)
+                // calculate the two light intensities (eq 1)
+                li1 = intensity(pop, i, r);
+                li2 = intensity(pop, j, r);
 
-                // evaluate and update the worst firefly in the population (eqn 4)
-
+                // only move the firefly if it is worse
+                if (li2 < li1)
+                {                  
+                    // move firefly i towards firefly j (equation 4)
+                    // (get a new position for a firefly)
+                    equationFour(pop, newPos, r, i, j);
+                    // evaluate and update the worst firefly in the population (eqn 4)
+                    addNewFirefly(pop, newPos);
+                }
             }
         }
+
+        cout << "Best fit[" << pop->getFunction() << "]: "
+                            << pop->getFitness(0) << "\t "
+                            << pop->getFitness(pop->getPopSize() / 2) << "\t  "
+                            << pop->getFitness(pop->getPopSize() - 1)
+                            << endl;
     }
+
+    delete[] newPos;
 }
 
 
@@ -67,19 +100,16 @@ void initializeFFO(Population* pop)
     // generate the fitness array
     pop->generateAllFitness();
 
+    // sort the population (and fitness array)
+    pop->sortPopulation();
 }
 
-
-void moveFirefly(Population* pop, const int ff1, const int ff2)
-{
-
-}
 
 // light intensity
 // needs to be fixed (figure out what original intensity means)
-double equationOne(Population* pop, const int r)
+double intensity(Population* pop, const int ff, double r)
 {
-    double intensity = pop->getFitness(r);
+    double intensity = pop->getFitness(ff);
     double gamma     = pop->getGamma();
 
     intensity *= exp(-gamma * pow(r, 2.0));
@@ -88,9 +118,9 @@ double equationOne(Population* pop, const int r)
 }
 
 // attractiveness
-double equationTwo(Population* pop, const int r)
+double equationTwo(Population* pop, double r)
 {
-    double beta = pop->getBeta();
+    double beta  = pop->getBeta();
     double gamma = pop->getGamma();
 
     beta *= exp(-gamma * pow(r, 2.0));
@@ -99,48 +129,96 @@ double equationTwo(Population* pop, const int r)
 }
 
 // distance between two fireflies (ff1, ff2)
-double equationThree(Population* pop, const int ff1, const int ff2)
+// produces the value r
+double ffDistance(Population* pop, const int ff1, const int ff2)
 {
-    double distance = 0; 
     double temp;
+    double sum = 0;
 
+    // create the summation of the square of the difference between two elements
     for (int i = 0; i < pop->getSolutionSize(); ++i)
     {
-        temp  = pop->getPopulation(ff1, i);
-        temp -= pop->getPopulation(ff2, i);
-
-        pow(temp, 2.0);
+        temp = pop->getPopulation(ff1, i) - pop->getPopulation(ff2, i);
+        temp = pow(temp, 2.0);
+        sum += temp;
     }
 
-    return pow(distance, 0.5);
+    // take the square root of the summation
+    return pow(sum, 0.5);
 }
 
-// movement
-double equationFour(Population* pop, const int r, const int ff1, const int ff2, const int t, const int d)
+// Create a new position
+void equationFour(Population* pop, double* newPos, const int r, const int ff1, const int ff2)
 {
     // set up random number generation
     random_device rd;
     mt19937 mt(rd());
-    uniform_real_distribution<double> distr(0, 1);
-
-    // retreive a random value and ensure it is not 0
+    normal_distribution<double>  distN(0,1);
     double rnd;
-    do {rnd = distr(mt);} while (rnd == 0.0);
-    
+
     // retreive and create other variables
-    double beta  = pop->getBeta();
-    double gamma = pop->getGamma();
-    double temp;
+    double alpha = pop->getAlpha();
+    double pos;
 
-    // calcualte the movement
-    temp  = pop->getPopulation(ff1, d);
-    temp += equationOne(pop, r);
-    temp *= pop->getPopulation(ff2, d) - pop->getPopulation(ff1, d);
+    // adjust the position of each dimension
+    for (int i = 0; i < pop->getSolutionSize(); ++i)
+    {
+        // retreive a random value and ensure it is not 0, then modify it
+        do {rnd = distN(mt);} while (rnd == 0.0);
+        rnd -= 0.5;
+        rnd *= pop->getUpperBound() - pop->getLowerBound();
 
-    // fix
-    temp += rnd * 1;
+        // calcualte the movement
+        pos  = pop->getPopulation(ff1, i);
+        pos += equationTwo(pop, r) * (pop->getPopulation(ff2, i) - pop->getPopulation(ff1, i));
+        pos += alpha * rnd;
 
-    
+        // check the bounds
+        if (pos > pop->getUpperBound()) pos = pop->getUpperBound();
+        if (pos < pop->getLowerBound()) pos = pop->getLowerBound();
+
+        // change the dimension's value
+        newPos[i] = pos;
+    }
+}
+
+void addNewFirefly(Population* pop, double* newPos)
+{
+    // get the new position's fitness
+    double newFit = runSolution(newPos, pop->getSolutionSize(), pop->getFunction());
+
+    // variables for the new position
+    int    index = 0;
+    double curFit;
+
+    // find the index to move it to, go from best to worst
+    while (index < pop->getPopSize()-1)
+    {
+        // get the fitness to compare to
+        curFit = pop->getFitness(index);
+
+        // if the current fitness is worse than the new one
+        if (newFit < curFit)
+            break;
+        else
+            ++index;
+    }
+
+    // going backwards, shift the worse fireflys down
+    for (int i = pop->getPopSize()-1; i > index; --i)
+    {
+        // replace each dimension with the one ahead of it
+        pop->setPopulation(i, pop->getPopulation(i-1));
+
+        // replace the fitnesses with the one before it
+        pop->setFitness(i, pop->getFitness(i-1));
+    }
+
+    // replace the values at the index
+    pop->setPopulation(index, newPos);
+
+    // replace the fitnes
+    pop->setFitness(index, newFit);
 }
 
 
