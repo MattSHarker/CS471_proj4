7cc0269236d1369999aa5416b40384a4358fa707
Matt Harker <mattsharker@gmail.com>
2019-05-15 20:36:01 -0700

    Added several functions to print csv files for all algorithms

--- include/csv.h
+++ include/csv.h
@@ -5,12 +5,23 @@
 #include "Population.h"
 #include "RecordKeeper.h"
 
-// CSVs for genetic algorithm
-void particleCSVs(Population** pops, RecordKeeper** recordKeeper);
-
-// void 
+// CSVs for Particle Swarm
+void particleCSVs(RecordKeeper** records);
 void particlePBestFit(RecordKeeper* rk, const int function);
 void particleGBestFit(RecordKeeper** records);
+void particleFuncCalls(RecordKeeper** records);
 
+// CSVs for Firefly Algorithm
+void fireflyCSVs(RecordKeeper** records);
+void fireflyHistBestFit(RecordKeeper** records);
+void fireflyHistWorstFit(RecordKeeper** records);
+void fireflyFuncCalls(RecordKeeper** records);
+
+// CSVs for Harmony Search
+void harmonyCSVs(RecordKeeper** records);
+void harmonyHistBestFit(RecordKeeper** records);
+void harmonyHistWorstFit(RecordKeeper** records);
+void harmonyFuncCalls(RecordKeeper** records);
+void harmonyTimeTaken(RecordKeeper** records);
 
 #endif
--- src/csv.cpp
+++ src/csv.cpp
@@ -20,7 +20,7 @@
 using namespace std;
 
 
-void particleCSVs(Population** pops, RecordKeeper** rks)
+void particleCSVs(RecordKeeper** rks)
 {
     cout << "creating Particle Swarm CSV files\n";
 
@@ -83,6 +83,220 @@ void particleGBestFit(RecordKeeper** rks)
         // write the rest of the experimentations
         for (int j = 1; j < rks[0]->getExperimentations(); ++j)
             csv << "," << rks[i]->getHistoricGBest(j);
+        
+        // add a newline
+        csv << '\n';
+    }
+
+    // close the csv file
+    csv.close();
+}
+
+
+void fireflyCSVs(RecordKeeper** rks)
+{
+    cout << "creating Firefly CSV files\n";
+
+    // create the CSVs
+    fireflyHistBestFit(rks);
+    fireflyHistWorstFit(rks);
+    fireflyFuncCalls(rks);
+
+    // print out a confirmation
+    cout << "csv files have been created for Firefly Algorithm\n";
+
+}
+
+void fireflyHistBestFit(RecordKeeper** rks)
+{
+     // set the filename (and path) for the csv
+    string pathName = "results/FFA/histBestFit/ffaBestFit.csv";
+
+    // create or open the csv
+    ofstream csv(pathName);
+    
+    //for each function, write the gBest
+    for (int i = 0; i < rks[0]->getNumFuncs(); ++i)
+    {
+        // write the first cost to prevent extra commas
+        csv << rks[i]->getHistoricBestFit(0);
+
+        // write the rest of the experimentations
+        for (int j = 1; j < rks[0]->getExperimentations(); ++j)
+            csv << "," << rks[i]->getHistoricBestFit(j);
+        
+        // add a newline
+        csv << '\n';
+    }
+
+    // close the csv file
+    csv.close();
+}
+
+void fireflyHistWorstFit(RecordKeeper** rks)
+{
+    // set the filename (and path) for the csv
+    string pathName = "results/FFA/histWorstFit/ffaWorstFit.csv";
+
+    // create or open the csv
+    ofstream csv(pathName);
+    
+    //for each function, write the gBest
+    for (int i = 0; i < rks[0]->getNumFuncs(); ++i)
+    {
+        // write the first cost to prevent extra commas
+        csv << rks[i]->getHistoricWorstFit(0);
+
+        // write the rest of the experimentations
+        for (int j = 1; j < rks[0]->getExperimentations(); ++j)
+            csv << "," << rks[i]->getHistoricWorstFit(j);
+        
+        // add a newline
+        csv << '\n';
+    }
+
+    // close the csv file
+    csv.close();
+}
+
+
+void fireflyFuncCalls(RecordKeeper** rks)
+{
+     // set the filename (and path) for the csv
+    string pathName = "results/FFA/funcCalls/ffaFuncCalls.csv";
+
+    // create or open the csv
+    ofstream csv(pathName);
+    
+    //for each function, write the gBest
+    for (int i = 0; i < rks[0]->getNumFuncs(); ++i)
+    {
+        // write the first cost to prevent extra commas
+        csv << rks[i]->getFinalFuncCalls(0);
+
+        // write the rest of the experimentations
+        for (int j = 1; j < rks[0]->getExperimentations(); ++j)
+            csv << "," << rks[i]->getFinalFuncCalls(j);
+        
+        // add a newline
+        csv << '\n';
+    }
+
+    // close the csv file
+    csv.close();
+}
+
+
+void harmonyCSVs(RecordKeeper** rks)
+{
+    cout << "creating Harmony CSV files\n";
+
+    // create the CSVs
+    harmonyHistBestFit(rks);
+    harmonyHistWorstFit(rks);
+    harmonyFuncCalls(rks);
+    harmonyTimeTaken(rks);
+
+    cout << "csv files have been created for Harmony Search Algorithm\n";
+}
+
+
+void harmonyHistBestFit(RecordKeeper** rks)
+{
+     // set the filename (and path) for the csv
+    string pathName = "results/HSA/histBestFit/hsaBestFit.csv";
+
+    // create or open the csv
+    ofstream csv(pathName);
+    
+    //for each function, write the gBest
+    for (int i = 0; i < rks[0]->getNumFuncs(); ++i)
+    {
+        // write the first cost to prevent extra commas
+        csv << rks[i]->getHistoricBestFit(0);
+
+        // write the rest of the experimentations
+        for (int j = 1; j < rks[0]->getExperimentations(); ++j)
+            csv << "," << rks[i]->getHistoricBestFit(j);
+        
+        // add a newline
+        csv << '\n';
+    }
+
+    // close the csv file
+    csv.close();
+}
+
+
+void harmonyHistWorstFit(RecordKeeper** rks)
+{
+     // set the filename (and path) for the csv
+    string pathName = "results/HSA/histWorstFit/hsaWorstFit.csv";
+
+    // create or open the csv
+    ofstream csv(pathName);
+    
+    //for each function, write the gBest
+    for (int i = 0; i < rks[0]->getNumFuncs(); ++i)
+    {
+        // write the first cost to prevent extra commas
+        csv << rks[i]->getHistoricWorstFit(0);
+
+        // write the rest of the experimentations
+        for (int j = 1; j < rks[0]->getExperimentations(); ++j)
+            csv << "," << rks[i]->getHistoricWorstFit(j);
+        
+        // add a newline
+        csv << '\n';
+    }
+
+    // close the csv file
+    csv.close();
+}
+
+void harmonyFuncCalls(RecordKeeper** rks)
+{
+     // set the filename (and path) for the csv
+    string pathName = "results/HSA/funcCalls/hsaFuncCalls.csv";
+
+    // create or open the csv
+    ofstream csv(pathName);
+    
+    //for each function, write the gBest
+    for (int i = 0; i < rks[0]->getNumFuncs(); ++i)
+    {
+        // write the first cost to prevent extra commas
+        csv << rks[i]->getFinalFuncCalls(0);
+
+        // write the rest of the experimentations
+        for (int j = 1; j < rks[0]->getExperimentations(); ++j)
+            csv << "," << rks[i]->getFinalFuncCalls(j);
+        
+        // add a newline
+        csv << '\n';
+    }
+
+    // close the csv file
+    csv.close();
+}
+
+void harmonyTimeTaken(RecordKeeper** rks)
+{
+    // set the filename (and path) for the csv
+    string pathName = "results/HSA/timeTaken/hsaTimeTaken.csv";
+
+    // create or open the csv
+    ofstream csv(pathName);
+    
+    //for each function, write the gBest
+    for (int i = 0; i < rks[0]->getNumFuncs(); ++i)
+    {
+        // write the first cost to prevent extra commas
+        csv << rks[i]->getTimeTaken(0);
+
+        // write the rest of the experimentations
+        for (int j = 1; j < rks[0]->getExperimentations(); ++j)
+            csv << "," << rks[i]->getTimeTaken(j);
         
         // add a newline
         csv << '\n';
