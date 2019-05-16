bba88db013f99cf9bde27452606cb2dd749e4875
Matt Harker <mattsharker@gmail.com>
2019-05-15 20:37:00 -0700

    Initial completed version of HSA

--- include/harmony.h
+++ include/harmony.h
@@ -3,12 +3,18 @@
 #define HARMONY_H
 
 #include "Population.h"
+#include "RecordKeeper.h"
 
-void harmony(Population** pops);
-void runHarmony(Population* pop);
+void harmony(Population** pops, RecordKeeper** records);
+void runHarmony(Population* pop, RecordKeeper* record);
 
 void initializeHS(Population* pop);
 
-void pitchAdjust(Population* pop, double* newHarm, const int originalHarmIndex);
+void pitchAdjust(Population* pop, double* newHarm, const int elem);
+double adjustPitch(double pitch, double bandwidth);
+
+void addNewHarmony(Population* pop, double* newHarmony, double newFitness);
 
 #endif
+
+
--- src/harmony.cpp
+++ src/harmony.cpp
@@ -4,35 +4,41 @@
 #include <thread>
 
 #include "harmony.h"
+#include "runFuncs.h"
 
 using namespace std;
 
-void harmony(Population** pops)
+void harmony(Population** pops, RecordKeeper** rks)
 {
     int funcs = pops[0]->getNumFuncs();
     thread* myThreads = new thread[funcs];
-/*
+
     for (int i = 0; i < funcs; ++i)
-        myThreads[i] = thread(runHarmony, pops[i]);
+        myThreads[i] = thread(runHarmony, pops[i], rks[i]);
     
     for (int i = 0; i < funcs; ++i)
         myThreads[i].join();
-*/
+
+/*
     for (int i = 0; i < funcs; ++i)
-        runHarmony(pops[i]);
+        runHarmony(pops[i], rks[i]);
+*/
 
     delete [] myThreads;
 }
 
-void runHarmony(Population* pop)
+void runHarmony(Population* pop, RecordKeeper* rk)
 {
     // initialize random number generation
     random_device rd;
     mt19937 mt(rd());
     uniform_real_distribution<double> distr(0, 1);
+    double rnd;
 
     // variables for the algorithm
     double* newHarm = new double[pop->getSolutionSize()];
+    double  newFit;
+    clock_t timer;
 
     // initialize population
     initializeHS(pop);
@@ -40,28 +46,44 @@ void runHarmony(Population* pop)
     // run it $experimentation times
     for (int i = 0; i < pop->getExperimentations(); ++i)
     {
-        // generate new harmonics by accepting new harmonics
+        // start the timer
+        timer = clock();
 
-        // adjust pitch to get new harmonics
+        // for each pitch in the harmony
+        for (int j = 0; j < pop->getSolutionSize(); ++j)
+        {
+            if (distr(mt) <= pop->getHMCR())
+            {
+                // choose an existing harmony randomly and set it to newHarm
+                rnd = distr(mt) * pop->getPopSize();
+                newHarm[j] = pop->getPopulation(rnd, j);
 
+                // adjust the pitch randomly within limits
+                if (distr(mt) <= pop->getPAR())
+                    newHarm[j] = adjustPitch(newHarm[j], pop->getBandwidth());
+            }
 
-        if (distr(mt) < pop->getHMCR())
-        {
-            // choose an existing harmony randomly
+            // otherwise generate new harmonics via randomization
+            else
+                newHarm[j] = pop->getLowerBound() + distr(mt) * (pop->getUpperBound() - pop->getLowerBound());
+
         }
-        else if (distr(mt) < pop->getPAR())
-        {
-            // adjust the pitch randomly within limits
-        }
-        else
-        {
-            // generate new harmonics via randomization
-        }
-        
         // accept the new solutions if better
+        newFit = runSolution(newHarm, pop->getSolutionSize(), pop->getFunction());
+        pop->incrimentFuncCalls();
 
-        // record best and worst solutions
+        if (newFit < pop->getFitness(pop->getPopSize()-1));
+            addNewHarmony(pop, newHarm, newFit);
 
+        // end the timer
+        timer = clock() - timer;
+       
+        // record data
+        rk->setHistoricBestFit(pop->getFitness(0), i);
+        rk->setHistoricWorstFit(pop->getFitness(pop->getPopSize()-1), i);
+        rk->setTimeTaken(double(timer*1000)/CLOCKS_PER_SEC, i);
+        rk->setFinalFuncCalls(pop->getFuncCalls(), i);
+        pop->resetFuncCalls();
     }
 
     delete[] newHarm;
@@ -87,10 +109,17 @@ void initializeHS(Population* pop)
 
     // sort the population (and fitness array)
     pop->sortPopulation();
+
+    if (pop->getFunction() == 0)
+        for (int i = 0; i < pop->getPopSize(); ++i)
+            cout << pop->getFitness(i) << endl;
+
+    // reset the function calls
+    pop->resetFuncCalls();
 }
 
 
-void pitchAdjust(Population* pop, double* newHarm, const int origIndex)
+void pitchAdjust(Population* pop, double* newHarm, const int elem)
 {
     // setup random number generation
     random_device rd;
@@ -101,7 +130,7 @@ void pitchAdjust(Population* pop, double* newHarm, const int origIndex)
     for (int i = 0; i < pop->getSolutionSize(); ++i)
     {
         // create the new value
-        double temp = pop->getPopulation(origIndex, i);
+        double temp = newHarm[elem];
         temp += pop->getBandwidth() * distr(mt);
 
         // assign the new value
@@ -109,5 +138,48 @@ void pitchAdjust(Population* pop, double* newHarm, const int origIndex)
     }
 }
 
+double adjustPitch(double pitch, double bandwidth)
+{
+    random_device rd;
+    mt19937 mt(rd());
+    uniform_real_distribution<double> distr(-1, 1);
 
+    return pitch + bandwidth * distr(mt);
+}
+
+void addNewHarmony(Population* pop, double* newHarm, double newFit)
+{
+    // variables for the new position
+    int    index = 0;
+    double curFit;
+
+    // find the index to move it to, go from best to worst
+    while (index < pop->getPopSize()-1)
+    {
+        // get the fitness to compare to
+        curFit = pop->getFitness(index);
+
+        // if the current fitness is worse than the new one
+        if (newFit < curFit)
+            break;
+        else
+            ++index;
+    }
+
+    // going backwards, shift the worse fireflys down
+    for (int i = pop->getPopSize()-1; i > index; --i)
+    {
+        // replace each dimension with the one ahead of it
+        pop->setPopulation(i, pop->getPopulation(i-1));
+
+        // replace the fitnesses with the one before it
+        pop->setFitness(i, pop->getFitness(i-1));
+    }
+
+    // replace the values at the index
+    pop->setPopulation(index, newHarm);
+
+    // replace the fitnes
+    pop->setFitness(index, newFit);
+}
 
